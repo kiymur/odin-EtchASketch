e7a9871ee3e7278fd644f4e579e7b9772b1e02ee
Kiyomi Murray <244055282+kiymur@users.noreply.github.com>
2026-05-06 14:48:36 -0400

    Add functionality to 'Set grid size' button
    * Convert event listener for hover to a function for easier repeated
    use
    * Remove height and width attribute from .grid elements in css and
    instead set width using for loop that creates the .grid elements in js
    * When the user clicks the 'Set grid size' button, if the user enters a
    number greater than 100 they are prompted again and asked to enter a
    number less than or equal to 100. If the number is less than or equal
    to 100 the grid adjusts so that the number of squares on each axis is
    equal to the user input while the container size remains the same

--- script.js
+++ script.js
@@ -3,19 +3,42 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 256; i++) {
     const i = document.createElement("div");
     i.classList.add("grid");
+    i.style.width = "40px"
     container.appendChild(i);
 }
 
-const grid = document.querySelectorAll(".grid");
+function hover() {
+    const grid = document.querySelectorAll(".grid");
 
-grid.forEach((item) => {
-    item.addEventListener("mouseover", (e) => {
-        e.target.style.background = "black";
+    grid.forEach((item) => {
+        item.addEventListener("mouseover", (e) => {
+            e.target.style.background = "black";
+        });
     });
-});
+}
+
+hover();
 
 const gridSizeBtn = document.querySelector("#gridSize");
 gridSizeBtn.addEventListener("click", (e) =>{
     let input = prompt("How many squares wide would you like the grid to be?");
+    input = parseFloat(input);
 
+    if (input > 100) {
+        return input = prompt("Please enter a number that is less than or equal to 100:")
+    } else {
+        container.innerHTML = '';
+
+        let newWidth = ((672/input) - 2);
+        newWidth = newWidth.toString() + "px";
+        
+        for (let i = 0; i < (input*input); i++) {
+            const i = document.createElement("div");
+            i.classList.add("grid");
+            i.style.width = newWidth;
+            container.appendChild(i);
+        }
+
+        hover();
+    }
 });
